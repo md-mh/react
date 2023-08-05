cbcb27ea193f49b3c09fb94b848d13ccb50db712
md-mh <mmahedi49@gmail.com>
2023-08-05 22:49:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Demo from './Demo';
+
+function AccordionItem({ title, content, isOpen, toggleAccordion }) {
+  return (
+    <div className="accordion-item">
+      <div className="accordion-header" onClick={toggleAccordion}>
+        {title}
+      </div>
+      {isOpen && <div className="accordion-content">{content}</div>}
+    </div>
+  );
+}
 
 function App() {
+  const accordionData = [
+    {
+      title: "Accordion Item 1",
+      content: "Content for Accordion Item 1",
+    },
+    {
+      title: "Accordion Item 2",
+      content: "Content for Accordion Item 2",
+    },
+    {
+      title: "Accordion Item 3",
+      content: "Content for Accordion Item 3",
+    },
+  ];
+
+  const [openIndex, setOpenIndex] = useState(0);
+
+  const toggleAccordion = (index) => {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="accordion">
+      {accordionData.map((item, index) => (
+        <AccordionItem
+          key={index}
+          title={item.title}
+          content={item.content}
+          isOpen={openIndex === index}
+          toggleAccordion={() => toggleAccordion(index)}
+        />
+      ))}
+      <AccordionItem
+        title='Test'
+        content={<Demo />}
+        isOpen={openIndex === 4}
+        toggleAccordion={() => toggleAccordion(4)}
+      />
     </div>
   );
 }
---
+++ src/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quidem cupiditate inventore eaque, quas saepe pariatur provident, facilis, minima harum laboriosam velit placeat accusantium facere. Ipsum debitis voluptas labore pariatur!
+        </div>
+    );
+};
+
+export default Demo;
